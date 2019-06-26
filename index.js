2727970f1acb86e8c00f35aeb0884509d0ad6f44
nolanjharris <thenolanjames@gmail.com>
2019-06-26 00:11:20 -0500

    completed clone of my GitHub Profile Page

---
+++ index.js
@@ -0,0 +1,12 @@
+// let months = ['Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'];
+
+// let tbl = document.createElement('table');
+// tbl.style.width = '100%';
+// let thead = document.createElement('thead');
+// let tbody = document.createElement('tbody');
+
+// function creatTable() {
+//     for(var i = 0; i < months.length; i++) {
+//         let th = document.createElement('th');
+//     }
+// }
